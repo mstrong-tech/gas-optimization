345a82d20b1ed49bfd1ee4105137a471ce1b09a6
MStrong <59189994+mstrong-tech@users.noreply.github.com>
2022-05-06 14:56:24 -0400

    Whitelist-Array,Mapping,Merkle

---
+++ contracts/WhitelistArray721.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WhitelistArray721 is ERC721, Ownable {
+    uint currTokenId = 1;
+    address[] whitelistedUsers;
+
+    constructor() ERC721("Token", "TKN"){
+    }
+
+    function mintPublicSale() external payable {
+        require(msg.value >= 0.2 ether, "Not enough ether");
+        _mint(msg.sender, currTokenId++);
+    }
+
+    function mintWhitelist() external payable {
+        require(isWhitelisted(msg.sender), "You are not whitelisted");
+        require(msg.value >= 0.1 ether, "Not enough ether");
+        _mint(msg.sender, currTokenId++);
+    }
+
+    function addToWhitelist(address user) external onlyOwner {
+        require(!isWhitelisted(user), "User is already whitelisted");
+        whitelistedUsers.push(user);
+    }
+
+    function isWhitelisted(address _user) public view returns (bool) {
+        for(uint i=0; i<whitelistedUsers.length; i++){
+            if(whitelistedUsers[i] == _user){
+                return true;
+            }   
+        }
+        return false;
+    }
+}
